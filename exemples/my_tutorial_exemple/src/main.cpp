51243db7e0e0650469746bf959914db53f9c4226
AndreFPDA <andrefelipepda@gmail.com>
2024-02-18 16:40:41 -0300

    correcao de bug para exibicao de mensagem pelo display e monitor serial

--- exemples/my_tutorial_exemple/src/main.cpp
+++ exemples/my_tutorial_exemple/src/main.cpp
@@ -17,36 +17,42 @@ void setup() {
     //setup display
     stdio_init_all();
 
-    i2c_init(i2c0, 400000);                 //Initialize I2C on i2c0 port with 400kHz
-    gpio_set_function(16, GPIO_FUNC_I2C);    //Use GPIO16 as I2C  SDA
-    gpio_set_function(17, GPIO_FUNC_I2C);    //Use GPIO17 as I2C  SCL
-    gpio_pull_up(16);                        //Pull up GPIO16 
-    gpio_pull_up(17);  
+    i2c_init(i2c1, 400000);                 //Initialize I2C on i2c0 or i2c1 port with 400kHz
+    gpio_set_function(14, GPIO_FUNC_I2C);    // as I2C  SDA
+    gpio_set_function(15, GPIO_FUNC_I2C);    // as I2C  SCL
+    gpio_pull_up(14);                        //Pull up GPIO16 
+    gpio_pull_up(15);  
 
 }
 
 int main() {
-    string mensagem = "COM-ACK\n\n";
+    string mensagem = "COM-ACK";
     setup();
 
-    oledfx oled(0x3C, size::W128xH64, i2c0);   //Declare oled instance
+    oledfx oled(0x3C, size::W128xH64, i2c1);   //Declare oled instance
     // if you are using 128x32 oled try size::W128xH32
+    oled.clear();
+    oled.drawString(0, 0, "Picolib");
+    oled.display();
 
-    printf("COM-ACK\n\n");
     getchar();
-
+    printf("COM?\n");
+    getchar();
     oled.clear();
     oled.drawString(0, 0, mensagem);
     oled.display();
-    sleep_ms(1000);
+    printf("COM-ACK\n");
 
     char serialImput;
 
     while (true) {
         serialImput = getchar();
-        oled.clear();
-        oled.drawString(0, 0, "Recebido:");
-        oled.drawChar(3, 10, serialImput);
-        oled.display();
+        if (serialImput != '\0') { //deiferente de vasio
+            oled.clear();
+            oled.drawString(0, 0, "Recebido:");
+            oled.drawChar(3, 10, serialImput);
+            oled.display();
+            printf("Recebido: %c\n", serialImput);
+        }
     }
 }
