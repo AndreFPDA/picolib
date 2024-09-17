34fbfac9790ee5d8ee20d9edd20ac1823ce4aad0
AndreFPDA <andrefelipepda@gmail.com>
2024-09-17 02:41:38 -0300

    Exemplo e lib para controle de N modulos DL1416.

---
+++ lib/DL1416/dlsiemens.hpp
@@ -0,0 +1,52 @@
+#ifndef	DLSIEMENS_h
+#define	DLSIEMENS_h
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <array>
+
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+namespace DL1416 {
+class DLSIEMENS {
+	public:
+	DLSIEMENS(const std::vector<int>& modulos, int wri,
+        int ds0, int ds1, int d0, int d1, int d2,
+        int d3, int d4, int d5, int d6);
+
+	//=================================================================================================
+    void characterSet(const char& character, const int& digit, const int& ModuleDisplay);
+	
+	//=================================================================================================
+	void writDisplay(const std::string&  str);
+
+	//=================================================================================================
+	void clearDisplay();
+	
+	//=================================================================================================
+	private:
+
+	const std::vector<int> _modulos;  
+	const int _wri;
+	const int _ds0;
+	const int _ds1;
+	const std::array<int, 7>  _dataCode;  
+	std::array<int, 128> charset;
+	
+	// Mapping the values ​​of _ds0 and _ds1 to each digit
+    const std::array<std::pair<int, int>, 4> digit_map = {{
+        {1, 1}, // Digit 0
+        {0, 1}, // Digit 1
+        {1, 0}, // Digit 2
+        {0, 0}  // Digit 3
+    }};
+
+	//=================================================================================================
+	void setDigit(const int& digit);
+
+};
+
+}//end namespace DL1416
+#endif
